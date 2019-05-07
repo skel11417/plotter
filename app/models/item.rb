0e749204f2d871cc114e8dd7b3eb7cbaea22e0d0
Sean <shonkelli@gmail.com>
2019-05-07 18:11:13 -0400

    updated seeds with basic items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,4 @@
 class Item < ApplicationRecord
   has_many :items_plots
   has_many :plots, through: :items_plots
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,21 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# {
-# id: 4,
-# url: "sample",
-# items: [
-# {
-# id: 2,
-# x: 246,
-# y: 200,
-# name: "mic stand",
-# image: "micstand.png"
-# },
-# {
-# id: 3,
-# x: 244,
-# y: 0,
-# name: "five-piece drumset",
-# image: "drumset-5.jpg"
-# },
-# {
-# id: 4,
-# x: 234,
-# y: 200,
-# name: "stage monitor",
-# image: "monitor.png"
-# }
-# ]
-# }
-Plot.destroy_all
+
+# Plot.destroy_all
 # Item.destroy_all
-p = Plot.create(url: 'sample')
+# p = Plot.create(url: 'sample')
 
 # items = [
-#   {name: 'mic stand', image: 'micstand.png'},
-#   {name: 'five-piece drumset', image: 'drumset-5.jpg'},
-#   {name: 'stage monitor', image: 'monitor.png'}
 # ]
+
+items = [ 
+  #   {name: 'mic-stand-boom', image: 'micstand-boom.png'},
+  #   {name: 'five-piece-drumset', image: 'drumkit-5.png'},
+  #   {name: 'stage monitor', image: 'monitor.png'}
+  {name: 'keyboard-single', image: 'keyboard-1.png'},
+  {name: 'bass-amp-large', image: 'bass-amp-large.png'},
+  {name: 'guitar-amp-stack', image: 'guitar-amp-1.png'},
+  {name: 'di box-mono', image: 'di-box-1.png'},
+  {name: 'power-strip-4', image: 'power-strip-4.png'}
+]
 items.each {|item| Item.create(item)}
