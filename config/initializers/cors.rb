9dfe8bb00f0377c945c2ffd3ae4c8cc0885b5bf0
Sean <shonkelli@gmail.com>
2019-07-07 22:31:43 -0400

    attempting to fix cors settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://stage-plotter.herokuapp.com/'
+    origins 'https://stage-plotter.herokuapp.com'
 
     resource '*',
       headers: :any,
